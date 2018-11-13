78fde60ea0772c2c505a601288a03e1a1bf87042
testes <joao4018@hotmail.com>
2018-11-13 17:38:50 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.7e9be457267164489dd4f9883f56a1b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testeDeploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ba011d451b055136fa7f",
+    "url": "/testeDeploy/static/js/runtime~main.ba011d45.js"
+  },
+  {
+    "revision": "72c4fa81f992380c92b4",
+    "url": "/testeDeploy/static/js/main.72c4fa81.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/testeDeploy/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "72c4fa81f992380c92b4",
+    "url": "/testeDeploy/static/css/main.0a7dd41c.chunk.css"
+  },
+  {
+    "revision": "e8a72c6741a10229d2ba9528f036dce5",
+    "url": "/testeDeploy/index.html"
+  }
+];
